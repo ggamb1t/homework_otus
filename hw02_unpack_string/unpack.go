3b7421bd4a3a6f43aba948ab71770d3abb5a6d01
Vladimir <50336576+ggamb1t@users.noreply.github.com>
2022-06-12 04:04:24 +0300

    done without asterisk

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,63 @@
 package hw02unpackstring
 
 import (
-	"errors"
+	"fmt"
+	"strconv"
+	"unicode"
+	"unicode/utf8"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+var (
+	ErrInvalidString = fmt.Errorf("invalid string")
+)
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	if str == "" {
+		return "", nil
+	}
+
+	if ok := utf8.ValidString(str); !ok {
+		return "", ErrInvalidString
+	}
+
+	var (
+		prevRune rune
+		lastRune rune
+		resBuf   = make([]byte, 0, 2*utf8.RuneCountInString(str))
+	)
+
+	for i, currRune := range str {
+		if i == 0 {
+			if unicode.IsDigit(currRune) {
+				return "", ErrInvalidString
+			}
+			if utf8.RuneCountInString(str) == 1 {
+				resBuf = utf8.AppendRune(resBuf, currRune)
+				return string(resBuf), nil
+			}
+			prevRune = currRune
+			continue
+		}
+
+		if unicode.IsDigit(currRune) && unicode.IsDigit(prevRune) {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(currRune) {
+			count, _ := strconv.Atoi(string(currRune))
+			for i := 0; i < count; i++ {
+				resBuf = utf8.AppendRune(resBuf, prevRune)
+			}
+		} else {
+			if !unicode.IsDigit(prevRune) {
+				resBuf = utf8.AppendRune(resBuf, prevRune)
+			}
+		}
+		prevRune = currRune
+		lastRune = currRune
+	}
+	if !unicode.IsDigit(lastRune) {
+		resBuf = utf8.AppendRune(resBuf, lastRune)
+	}
+	return string(resBuf), nil
 }
