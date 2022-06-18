b48394483ccb0c056754649c35ed51e6fd74020d
Vladimir <50336576+ggamb1t@users.noreply.github.com>
2022-06-18 07:47:14 +0300

    hw03 WIP - order is essential? why?

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,46 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(str string) []string {
+	frequencyMap := make(map[string]int64)
+	result := make([]string, 0, 10)
+
+	words := strings.Split(str, " ")
+
+	for _, word := range words {
+		if word == " " || word == "" {
+			continue
+		}
+		if _, ok := frequencyMap[word]; !ok {
+			frequencyMap[word] = 1
+		} else {
+			frequencyMap[word]++
+		}
+	}
+	type mapToSlice struct {
+		Key   string
+		Value int64
+	}
+	var values []mapToSlice
+	for key, value := range frequencyMap {
+		values = append(values, mapToSlice{key, value})
+	}
+
+	sort.Slice(values, func(i, j int) bool {
+		return values[i].Value > values[j].Value
+	})
+
+	for i := range values {
+		if i < 10 {
+			result = append(result, values[i].Key)
+		} else {
+			break
+		}
+	}
+
+	return result
 }
